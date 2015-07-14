a8bc7b41ed421f15ef0044f28af56f22b1a79b61
camello <cristian.moreno.chavez@gmail.com>
2015-07-13 18:04:23 -0600

    version 2

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,3 @@
+exports.author = function(req,res){
+	res.render('author',{nombre:'Cristian Moreno', ocupacion:'Desarrollador de software',foto:'foto.jpg'});
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require("../controllers/quiz_controller.js");
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Estamos en Heroku' });
 });
+
+router.get('/author',quizController.author);
 
 module.exports = router;
